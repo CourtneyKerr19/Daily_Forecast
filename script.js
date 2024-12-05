757cee25a8d2e6a0ad58af6cbbfb8c00af0a8bce
Courtney Kerr <courtneykerr219@gmail.com>
2024-12-05 01:27:18 -0600

    Added forcastUrl and added updated getWeather function with built in error message.

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const apiKey = '616d568b21e6502c2bcf3499b1b78e15';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast';
+
+function getWeather() {
+  const cityName = document.getElementById('city').value;
+
+  if(!cityName) {
+    alert('Please enter a city name');
+    return;
+  }
+}
