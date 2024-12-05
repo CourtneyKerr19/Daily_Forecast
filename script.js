a1c873899fccd6ec31a3a6192895bcd7296b516f
Courtney Kerr <courtneykerr219@gmail.com>
2024-12-05 01:54:46 -0600

    Incorporate async function to fetch weather data from the API.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 const apiKey = '616d568b21e6502c2bcf3499b1b78e15';
+
+async function getWeather(city) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+    );
+    const data = await response.json();
+    displayWeather(data);
+
+    const hourlyResponse = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`
+    );
+    const hourlyData = await hourlyResponse.json();
+    displayHourlyForecast(hourlyData.list.slice(0, 5));
+  } catch (error) {
+    console.log('Error fetching data', error);
+  }
+}
